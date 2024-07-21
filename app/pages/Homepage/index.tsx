d03f99484d3ec99892545acb4577f3dadb21465c
abeltruong <abel.t@beginningboutique.com.au>
2024-07-21 17:39:11 +0700

    about us section

--- app/pages/Homepage/index.tsx
+++ app/pages/Homepage/index.tsx
@@ -31,6 +31,28 @@ function HomePage() {
           </Link>
         </div>
       </div>
+
+      {/* About us */}
+      <div className="container bb__about pt-20 flex flex-col gap-4 lg:gap-6 items-center">
+        <p className="text-lg lg:text-xl text-secondary">Một câu chuyện</p>
+        <h1 className="">Về chúng tôi</h1>
+        <p className="text-sm max-w-[500px] leading-8 text-center">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-4 lg:gap-5 mt-4">
+          <div className="">
+            <img src="https://anatolia.vn/wp-content/uploads/2024/04/doner.webp" alt="about us" />
+          </div>
+          <div className="">
+            <img src="https://anatolia.vn/wp-content/uploads/2024/04/doner.webp" alt="about us" />
+          </div>
+          <div className="">
+            <img src="https://anatolia.vn/wp-content/uploads/2024/04/doner.webp" alt="about us" />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
